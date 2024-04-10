4dae281cb449d8a8d07ad14fbe9a106cb2b95a26
Stephanie Dennehy <stephanie_dennehy@icloud.com>
2024-04-10 14:37:08 -0400

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
+const navbar = document.getElementById('navbar');
 
+window.onscroll = function() {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove('top');
+  } else {
+    navbar.classList.add('top')
+  }
+}
 
+$('#navbar a, .btn').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+
+})
